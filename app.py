bb584255b6f5873e37561beab65c7d535e74706b
YarlValle <83057590+YarlValle@users.noreply.github.com>
2021-05-29 18:20:59 +0300

    test

--- app.py
+++ app.py
@@ -1,54 +1,44 @@
-from kivy.uix.screenmanager import Screen
-from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton #Прямоугольная Плоская кнопка MD
-from kivy.lang import Builder #Конструктор типа Lang 
-from kivy.factory import Factory
-from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-from kivy.core.window import Window #Для размера окна
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: 'Goto settings'
+            on_press: root.manager.current = 'settings'
+        Button:
+            text: 'Quit'
 
-Window.size = (480, 853)
+<SettingsScreen>:
+    BoxLayout:
+        Button:
+            text: 'My settings button'
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+""")
 
+# Declare both screens
+class MenuScreen(Screen):
+    pass
 
-KV = '''
-MDScreen:
+class SettingsScreen(Screen):
+    pass
 
-    FitImage:
-        source:'fS6tmUgBPmw.jpg'
-    
-    
+class TestApp(App):
 
-    MDRaisedButton:
-        text: "Кнопка Светлее"
-        pos_hint: {"center_x": 0.5, "center_y": 0.7}
-        md_bg_color: app.theme_cls.primary_light
-
-    MDRaisedButton:
-        text: "Обычный цвет"
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-
-    MDRaisedButton:
-        text: "Тёмный цвет"
-        pos_hint: {"center_x": 0.5, "center_y": 0.3}
-        md_bg_color: app.theme_cls.primary_dark
-
-    MDRectangleFlatButton:
-        text: "Прозрачная кнопка"
-        pos_hint: {"center_x": .2, "center_y": .6} 
-        md_bg_color: app.theme_cls.primary_light   
-'''
-
-class MainApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "BlueGray" #Основная палтра
-        self.theme_cls.theme_style = "Light"           #Общий фон
-        
-        self.root = Builder.load_string(KV)           # Исполняет код в KV
-        return Builder.load_string(KV)
-    
-    def on_start(self):
-        self.fps_monitor_start()
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(SettingsScreen(name='settings'))
 
-        
-        
-MainApp().run()
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
--- main1.py
+++ main1.py
@@ -5,15 +5,17 @@ from kivymd.uix.button import MDRectangleFlatIconButton
 from kivymd.uix.label import MDLabel
 from kivy.lang import Builder
 from kivymd.uix.button import MDRaisedButton #Прямоугольная Плоская кнопка MD
-
+import requests
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window #Для размера окна
 from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import ScreenManager, Screen
 Window.size = (480, 600)
+# Create the manager
 
-KV="""
-СС:
+Builder.load_string("""
+<Autorith>:
     send_fone: fone
     ch: but
     MDScreen:
@@ -31,7 +33,6 @@ KV="""
                 orientation: 'vertical'
 
             
-                
                 
                 MDTextField:
                     id: fone
@@ -51,7 +52,8 @@ KV="""
                     icon: "android"
                     theme_text_color: "Custom"
                     on_release:
-                        root.send()  
+                        root.send()
+                        root.manager.current = 'code'    
 
             BoxLayout:
                 orientation: 'vertical'
@@ -65,47 +67,102 @@ KV="""
                 orientation: 'vertical'        
             BoxLayout:
                 orientation: 'vertical'
-"""
-KV2 = '''
-Code:
+<Code>:
+    send_cod: cod
+    ch: but
     MDScreen:
         FitImage:
             source: 'fS6tmUgBPmw.jpg'
-'''
-class Code(BoxLayout):
+
+        GridLayout:
+            rows: 10
+
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+
+                MDTextField:
+                    id: cod
+                    hint_text: "Введите код из сообщения"
+                    line_color_focus: 1, 0, 1, 1
+                    multiline: False
+                    size_hint: .8 , .1
+                    pos_hint: {'center_x':0.5,'center_y':0.7}
+            BoxLayout:
+                orientation: 'vertical'    
+                MDTextButton:
+                    id: but
+                    pos_hint: {'center_x':0.5,'center_y':0.5}
+                    size_hint: 0.5,0.1
+                    text: "Подтвердить"
+                    font_size:  '25sp'
+                    icon: "android"
+                    theme_text_color: "Custom"
+                    on_release:
+                        root.sendcode()
+
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'        
+            BoxLayout:
+                orientation: 'vertical'
+<Menu>:
+    send_cod: cod
+    ch: but
+    MDScreen:
+        FitImage:
+            source: 'fS6tmUgBPmw.jpg'
+""")
+
+class Menu(Screen):
     pass
 
-class СС(MDApp,BoxLayout):
-    fl = 0
+class Code(Screen):
+    def sendcode(self):
+        hash1=""
+        hash2=""
+        #requests.get('htt://45.128.207.185:8000',params={'phone':fone})
+        #self.send_fone.hint_text="Введите код"
+        #self.send_fone.text=""
+        hash2=""
+        sm.switch_to(Menu(name='menu'))
+
+class Autorith(Screen):
     def send(self):
+        hash1=""
+        hash2=""
         fone = self.send_fone.text
         self.ch.text="Подтвердить"
+        #requests.get('htt://45.128.207.185:8000',params={'phone':fone})
         self.send_fone.hint_text="Введите код"
         self.send_fone.text=""
-        self.theme_cls.primary_palette = "BlueGray" #Основная палтра
-        self.theme_cls.theme_style = "Light"           #Общий фон
+        hash2=""
+        sm.switch_to(Code(name='code'))
         
-        self.root = Builder.load_string(KV2)           # Исполняет код в KV
-        return Builder.load_string(KV2)  
-
-
-#class MainApp(MDApp):
-#    def build(self):
-#        return СС()
-#class MainApp(MDApp):
-#    def build(self):
-#        self.root = Builder.load_string(KV)
-    
-#MainApp().run()
+sm = ScreenManager()    
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "BlueGray" #Основная палтра
         self.theme_cls.theme_style = "Light"           #Общий фон
         
-        self.root = Builder.load_string(KV)           # Исполняет код в KV
-        return Builder.load_string(KV)
+        sm.add_widget(Autorith(name='autorith'))
+        #sm.add_widget(Code(name='code'))
+        sm.current = 'autorith'
+        return sm
+
+    
 
 
         
-        
-MainApp().run()
+if __name__ == '__main__':     
+    MainApp().run()
