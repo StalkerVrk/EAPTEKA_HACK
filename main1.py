dbabd7a64f66c13bd6216449985b74f4507bb13c
YarlValle <83057590+YarlValle@users.noreply.github.com>
2021-05-29 20:53:01 +0300

    rt

--- main1.py
+++ main1.py
@@ -1,4 +1,4 @@
-
+import hashlib
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatIconButton
@@ -10,63 +10,75 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window #Для размера окна
 from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import ScreenManager, Screen
-Window.size = (380, 500)
+from kivy.uix.screenmanager import ScreenManager, Screen, TransitionBase
+from kivy.clock import Clock
+Window.size = (480, 600)
 # Create the manager
-
 Builder.load_string("""
 <Hi>:
     MDScreen:
+        on_enter: 
+            root.manager.current = 'autorith'
         FitImage:
-            source: 'Dawn.jpg'
+            source: 'fS6tmUgBPmw.jpg'
+            
+
+
 <Autorith>:
     send_fone: fone
     ch: but
-
-    MDFloatLayout:
-        MDScreen:
-            FitImage:
-                source: 'Dawn.jpg'
-        
-        MDLabel:
-            text: "Login"
-            pos_hint: {"center_y": .85}
-            font_style: "H4"
-            halign: "center"
-            theme_text_color: "Custom"
-            text_color: 0,0,0,1
-        
-        MDLabel:
-            text: "Welcome To EAPTEKA"
-            pos_hint: {"center_y": .75}
-            font_style: "H5"
-            halign: "center"
-            theme_text_color: "Custom"
-            text_color: 0,0,0,1
-        
-        MDTextField:
-            id: fone
-            hint_text: "Enter Your Number"
-            pos_hint: {"center_x": .5, "center_y": .55}
-            current_hint_text_color: 0,0,0,1
-            size_hint_x: .8
-        
-        MDRaisedButton:
-            id: but
-            text: "Input"
-            pos_hint: {"center_x": .5, "center_y": .3}
-            size_hint_x: .5
-            on_release:
-                root.send()
-                root.manager.current = 'code'
-            
-
-<Code>:
-    send_cod: cod
-    ch: but
     MDScreen:
         FitImage:
-            source: 'Dawn.jpg'
+            source: 'fS6tmUgBPmw.jpg'
+
+        GridLayout:
+            rows: 10
+
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+
+                MDTextField:
+                    id: fone
+                    hint_text: "Введите номер телефона"
+                    line_color_focus: 1, 0, 1, 1
+                    multiline: False
+                    size_hint: .6 , .1
+                    pos_hint: {'center_x':0.5,'center_y':0.7}
+            BoxLayout:
+                orientation: 'vertical'    
+                MDTextButton:
+                    id: but
+                    pos_hint: {'center_x':0.5,'center_y':0.5}
+                    size_hint: 0.5,0.1
+                    text: "Войти"
+                    font_size:  '25sp'
+                    icon: "android"
+                    theme_text_color: "Custom"
+                    on_release:
+                        root.send()
+                        root.manager.current = 'code'    
+
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'
+            BoxLayout:
+                orientation: 'vertical'        
+            BoxLayout:
+                orientation: 'vertical'
+<Code>:
+    send_cod: cod
+    MDScreen:
+        FitImage:
+            source: 'fS6tmUgBPmw.jpg'
 
         GridLayout:
             rows: 10
@@ -83,12 +95,11 @@ Builder.load_string("""
                     hint_text: "Введите код из сообщения"
                     line_color_focus: 1, 0, 1, 1
                     multiline: False
-                    size_hint: .8 , .1
+                    size_hint: .6 , .1
                     pos_hint: {'center_x':0.5,'center_y':0.7}
             BoxLayout:
                 orientation: 'vertical'    
                 MDTextButton:
-                    id: but
                     pos_hint: {'center_x':0.5,'center_y':0.5}
                     size_hint: 0.5,0.1
                     text: "Подтвердить"
@@ -97,6 +108,7 @@ Builder.load_string("""
                     theme_text_color: "Custom"
                     on_release:
                         root.sendcode()
+                        root.manager.current = 'menu'
 
             BoxLayout:
                 orientation: 'vertical'
@@ -111,45 +123,54 @@ Builder.load_string("""
             BoxLayout:
                 orientation: 'vertical'
 <Menu>:
-    send_cod: cod
-    ch: but
+    
     MDScreen:
         FitImage:
-            source: 'Dawn.jpg'
+            source: 'fS6tmUgBPmw.jpg'
 """)
+sm = ScreenManager()
+
+hesh1=''
+hesh2=''
 
 class Menu(Screen):
     pass
 
 class Code(Screen):
     def sendcode(self):
-        hash1=""
-        hash2=""
-        #requests.get('htt://45.128.207.185:8000',params={'phone':fone})
-        #self.send_fone.hint_text="Введите код"
-        #self.send_fone.text=""
-        #hash2=""
+        cd = self.send_cod.text
+
         sm.switch_to(Menu(name='menu'))
+
+        
 
 class Autorith(Screen):
     def send(self):
-        hash1=""
-        hash2=""
         fone = self.send_fone.text
-        self.ch.text="Подтвердить"
-        #requests.get('htt://45.128.207.185:8000',params={'phone':fone})
-        self.send_fone.hint_text="Введите код"
-        self.send_fone.text=""
-        hash2=""
+        hesh1 = requests.get('http://45.128.207.185:8000',params={'phone':fone})
         sm.switch_to(Code(name='code'))
+
+
+
+#class Hi(Screen):
+#    def no_args_func(self):
+#        while(1):
+#            if(3==Clock.get.fps):
+#                print('t')
+#                sm.add_widget(Autorith(name='autorith'))
+#                sm.current = 'hi' 
+#                TransitionBase.start(sm)
+
         
-sm = ScreenManager()    
+
+
 class MainApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "Blue" #Основная палтра
+        self.theme_cls.primary_palette = "BlueGray" #Основная палтра
         self.theme_cls.theme_style = "Light"           #Общий фон
         
         sm.add_widget(Autorith(name='autorith'))
+        #sm.add_widget(Hi(name='hi'))
         #sm.add_widget(Code(name='code'))
         sm.current = 'autorith'
         return sm
